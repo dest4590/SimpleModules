53b72265ad3244621c4a35177d284a350d1743aa
purpl3-yt <remezdevid2000@gmail.com>
2023-07-23 00:28:46 +0300

    Add Framework

---
+++ SimpleFramework.py
@@ -0,0 +1,20 @@
+# Помощник для моих модулей
+from telebot import types
+
+# Жирный текст
+bold = lambda text: '<b>' + text + '</b>'
+
+# Моно текст
+mono = lambda text: '<code>' + text + '</code>'
+
+# Получить аргументы из сообщения
+def getArgs(message: types.Message):
+    return message.text.split(' ')[1:]
+
+# Проверить аргументы
+def checkArgs(message: types.Message, index: int):
+    try:
+        getArgs(message)[index]
+        return True
+    except IndexError:
+        return False
